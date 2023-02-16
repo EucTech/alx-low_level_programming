d7cefd3b8fd2629848ed34be9de51ef717071065
EucTech <euccreative@gmail.com>
2023-02-16 23:25:57 +0000

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - To display the sizeof the datas
+ * Return: Always
+ */
+int main(void)
+{
+int a = 16;
+	printf("Size of variable a : %d", sizeof(a));
+	printf("Size of int data type : %d", sizeof(int));
+	printf("Size of char data type : %d", sizeof(char));
+	printf("Size of float data type : %d", sizeof(float));
+	printf("Size of double data type : %d", sizeof(double));
+	return (0);
+}
