8e0d1ce124c0651feca645a7ff60222144b7e67f
EucTech <euccreative@gmail.com>
2023-02-16 22:19:10 +0000

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main-Entry point
  * Return: Always
  */
 int main(void) 
 {
 puts("\"Programming is like building a multilingual puzzle");
-return(0);
+return (0);
 }
