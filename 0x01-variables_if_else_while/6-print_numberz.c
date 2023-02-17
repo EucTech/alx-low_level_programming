287b2d60c59e901d003013ee5a41074b457a1dca
EucTech <euccreative@gmail.com>
2023-02-17 23:34:24 +0000

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - To print signle numbers of base 10 using putchar
+ * Return: Always
+ */
+
+int main(void)
+{
+	int s_num;
+
+	for (s_num = 0; s_num < 19; s_num++)
+	{
+		putchar(s_num);
+	}
+
+	puchar('\n');
+
+	return (0);
+}
