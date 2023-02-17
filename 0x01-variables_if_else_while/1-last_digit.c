dcd97e984dedb3e09b7af1ceb6cc0be27f835b3e
EucTech <euccreative@gmail.com>
2023-02-17 21:07:44 +0000

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -7,24 +7,24 @@
  */
 int main(void)
 {
-	int n, last_digit;
+	int n, last_dt;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	last_digit = n % 10;
+	last_dt = n % 10;
 
-	if (last_digit > 5)
+	if (last_dt > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
+		printf("Last digit of %d is %d and is greater than 5\n", n, last_dt);
 	}
-	else if (last_digit == 0)
+	else if (last_dt == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", n, last_digit);
+		printf("Last digit of %d is %d and is 0\n", n, last_dt);
 	}
 	else
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_dt);
 	}
 	return (0);
 }
