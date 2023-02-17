584d331175e9c29d13f251c1c46b79c82f300f23
EucTech <euccreative@gmail.com>
2023-02-17 22:15:48 +0000

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - To print alphabet in lower and uppercase
+ * Return: Always
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a''Z'; i <= 'a''Z'; i++)
+	{
+		putchar(i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
