81f9ada536c02998ebf7972c6e3b82eb6a3decb3
EucTech <euccreative@gmail.com>
2023-02-17 23:18:49 +0000

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - To print single digit numbers of base 10 starting from o
+ * Return: Alway
+ */
+
+int main(void)
+{
+	int s_num;
+
+	for (s_num = 0; s_num >= 10; s_num++)
+	{
+		printf("%d\n", s_num);
+	}
+
+	return (0);
+}
