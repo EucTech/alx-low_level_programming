047f715fe475982a2a42e0d91e987487f0e738b5
EucTech <euccreative@gmail.com>
2023-02-17 20:36:13 +0000

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,10 +19,12 @@ int main(void)
 	if (last_digit > 5)
 	{
 		printf("%d and is greater than 5\n", last_digit);
-	} else if (last_digit == 0)
+	}
+	else if (last_digit == 0)
 	{
 		printf("%d and is 0\n", last_digit);
-	} else if(last_digit < 6)
+	}
+	else
 	{
 		printf("%d and is less than 6 and not 0\n", last_digit)
 	}
