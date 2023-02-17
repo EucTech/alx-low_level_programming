4bbda9b05398095cd222ac4028661d3933f3c055
EucTech <euccreative@gmail.com>
2023-02-17 00:24:51 +0000

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,9 +5,11 @@
  */
 int main(void)
 {
-	printf("Size of int : %lu\n", sizeof(int));
-	printf("Size of char : %lu\n", sizeof(char));
-	printf("Size of float : %lu\n", sizeof(float));
-	printf("Size of double : %lu\n", sizeof(double));
+	printf("Size of int : %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int : %lu byte(s)\n", sizeof(long int));
+	printf("Size of long long : %lu byte(s)\n", sizeof(long long));
+	printf("Size of char : %lu byte(s)\n", sizeof(char));
+	printf("Size of float : %lu byte(s)\n", sizeof(float));
+	printf("Size of double : %lu byte(s)\n", sizeof(double));
 	return (0);
 }
