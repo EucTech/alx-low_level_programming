cf33a353f9bb8059b94e3cd42a30fed977bce12f
EucTech <euccreative@gmail.com>
2023-02-18 02:12:16 +0000

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - To print combination of two digits
+ * Return: Always
+ */
+
+int main(void)
+{
+
+	int i, k;
+
+	for (i = 0; i < 9; i++)
+	{
+	for (k = i + 1; k < 10; k++)
+	{
+		putchar((i % 10) + '0');
+		putchar((k % 10) + '0');
+
+		if (i == 8 && k == 9)
+		continue;
+
+		putchar(',');
+		putchar(' ');
+	}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
