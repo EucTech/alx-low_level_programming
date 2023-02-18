f0aa1dc00a9c0a7a7cb2d8287a3b05559f322cca
EucTech <euccreative@gmail.com>
2023-02-18 16:52:48 +0000

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -19,20 +19,12 @@ int main(void)
 		putchar((b % 10) + '0');
 
 		putchar(' ');
-	}
-	for (a = 0; a < 100; a++)
-	{
-	for (b = 0; b < 99; b++)
-	{
-		if (a == 99 && a == 98)
-		continue;
 
 		putchar((a % 10) + '0');
 		putchar((b % 10) + '0');
 
 		putchar(',');
 		putchar(' ');
-	}
 	}
 	}
 
