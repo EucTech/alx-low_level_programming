6fac54328275fb0000bf511b56c35c742b334df8
EucTech <euccreative@gmail.com>
2023-02-21 15:44:25 +0000

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
+#include <main.h>
 
-/**main - to print _putchar
-  *Return: Alway
+/**
+  * main - to print _putchar
+  * Return: Alway
   */
 
 int main(void)
 {
-	_putchar("\"");
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
 
 	_putchar('\n');
 
