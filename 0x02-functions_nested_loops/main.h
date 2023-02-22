5416447101aa27a9ecc3034ee5c9d876e6d4b71d
EucTech <euccreative@gmail.com>
2023-02-22 01:08:49 +0000

    7-last_digit

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
 #ifndef MAIN_H
-#define MAIN_H 
+#define MAIN_H
 
 int _putchar(char c);
 void print_alphabet(void);
@@ -8,5 +8,6 @@ int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
+int print_last_digit(int);
 
 #endif
