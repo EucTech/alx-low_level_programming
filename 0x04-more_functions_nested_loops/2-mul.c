6a4a51d0d8730ca4f33ee0d2d5ab9817d2f01cc5
EucTech <euccreative@gmail.com>
2023-02-23 22:59:05 +0000

    2-m

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - write a function that multiplies two integer
+ * @a: integer 1
+ * @b: integer 2
+ * Return: Always
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
