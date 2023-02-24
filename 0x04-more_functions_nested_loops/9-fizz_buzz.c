50ce960ace33b215a90e3b07e51deabd518a4fa4
EucTech <euccreative@gmail.com>
2023-02-24 21:57:33 +0000

    9-fizz_buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the numbers from 1 to 100
+ * Return: Always
+ */
+
+int main(void)
+{
+	int i;
+	char f[] = "FIZZ";
+	char b[] = "BUZZ";
+	char fb[] = "FIZZBUZZ";
+
+	for (i = 1; i <= 100; i++)
+	{
+	if (i == 100)
+	{
+		printf("%s", b);
+	}
+	else if ((i % 3 == 0) && (i % 5 == 0))
+	{
+		printf("%s ", fb);
+	}
+	else if (i % 3 == 0)
+	{
+		printf("%s ", f);
+	}
+	else if (i % 5 == 0)
+	{
+		printf("%s ", b);
+	}
+	else
+	{
+		printf("%d ", i);
+	}
+	}
+	printf("\n");
+	return (0);
+}
