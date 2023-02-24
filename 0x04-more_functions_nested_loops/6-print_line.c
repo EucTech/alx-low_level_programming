da6a85470f889d055b93b989ad1a15ceb0f83549
EucTech <euccreative@gmail.com>
2023-02-24 12:26:40 +0000

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_line - write a function that draws a straight line in the terminal
+ * Return: Always
+ * @n: the parameter
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+	for (i = 1; i <= n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+	}
+}
