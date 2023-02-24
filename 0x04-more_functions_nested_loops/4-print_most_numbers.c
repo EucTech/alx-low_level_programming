c3528f061c3793b7070f93c9982d078dedd3d688
EucTech <euccreative@gmail.com>
2023-02-24 01:54:46 +0000

    4-print_most_numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_most_numbers - To write a function that prints 0 to 9 expect 2 and 4
+ * Return: Always
+ */
+
+void print_most_numbers(void)
+{
+	int k;
+
+	k = 0;
+
+	while (k < 10)
+	{
+	if (k != 2 &&k != 4)
+	{
+		_putchar(k + '0');
+	}
+		k++;
+	}
+		_putchar('\n');
+}
