07f86bdc7b2f086a2b995eada4eeaed2b31e2ff6
EucTech <euccreative@gmail.com>
2023-02-28 02:37:39 +0000

    9-strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * *_strcpy - to write a function that copies the string
+ * @dest: first
+ * @src: second
+ * Return: Always
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	char *guard = dest;
+
+	while (*src++)
+	{
+		*dest++ = *src++;
+		*dest = 0;
+	}
+	return (guard);
+}
