b45eaa823a26c39e234d638eb382e372fb423a75
EucTech <euccreative@gmail.com>
2023-02-28 02:09:15 +0000

    7-puts_half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * puts_half - a funtion that prints half of string
+ * @str: string
+ * Return: Always
+ */
+
+void puts_half(char *str)
+{
+	int n, m, l;
+
+	l = 0;
+
+	for (n = 0; str[n] != '\0'; n++)
+	{
+		l++;
+
+		m = (l / 2);
+	}
+
+	if ((l % 2) == 1)
+	{
+		m = ((l + 1) / 2);
+	}
+
+	for (n = m; str[n] != '\0'; n++)
+	{
+		_putchar(str[n]);
+	}
+	_putchar('\n');
+}
