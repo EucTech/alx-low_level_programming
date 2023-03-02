436ba29bfcbe2dd71447cd09ed41b27138da9e83
EucTech <euccreative@gmail.com>
2023-03-02 13:21:11 +0000

    0-strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * *_strcat - To write a function that concatenates two strings
+ * @dest: fisrt string
+ * @src: second string
+ * Return: Alwys
+ */
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest, src);
+
+	return (dest);
+}
