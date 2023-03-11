2d0bef2712e26dacf8eed666442e2d38af9abe9e
EucTech <euccreative@gmail.com>
2023-03-11 01:45:24 +0000

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include "stdio.h"
+#include "stdlib.h"
+/**
+ * main - a program that multiplies two numbers
+ * @argc: is the number of command line arguments
+ * @argv: an array containing the program CMD line Arguments
+ * Return: 1
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, mul = 1;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		mul *= atoi(argv[i]);
+	}
+	
+	printf("%d\n", mul);
+	return (0);
+}
