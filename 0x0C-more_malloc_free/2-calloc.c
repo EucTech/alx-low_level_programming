4683ed0d2feb0694c29ec475b9539b173678d759
EucTech <euccreative@gmail.com>
2023-03-17 01:54:54 +0000

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -24,7 +24,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 
 	for (i = 0; i < nmemb * size; i++)
-		*((char*)arr + i) = 0;
+		*((char *)arr + i) = 0;
 
 	return (arr);
 }
