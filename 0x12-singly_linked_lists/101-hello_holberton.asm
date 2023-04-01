965f4fc10346aeb92f6c8e71332d3c744e7471c2
EucTech <euccreative@gmail.com>
2023-04-01 22:14:02 +0000

    assembly code that print a string

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,10 +1,20 @@
-	global main
-	extern puts
+section .data
+	nl db 10, 0 
+	msg db "Hello, Holberton", 0
 
-msg:	db	"Hello, Holberton",0
+section .text
+	global main
+	extern printf
 
 main:
-	mov	rdi, msg
-	call	puts
-	mov	rax, 0
+	push rbp
+	mov rbp, rsp
+	mov rdi, msg
+	xor eax, eax
+	call printf
+	mov rdi, nl
+	call printf
+	mov rsp, rbp
+	pop rbp
+	xor eax, eax
 	ret
