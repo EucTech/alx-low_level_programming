cea108357a98de92d090f72b16b64e72da89d374
EucTech <euccreative@gmail.com>
2023-04-03 22:49:51 +0000

    freed a memory in a list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - a function that frees a list
+ * @head: The head
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (*head != NULL)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+	}
+}
