14051395d9b3073589985ea1489b9f1980d60bfc
EucTech <euccreative@gmail.com>
2023-04-04 21:50:14 +0000

    fixed code an error

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -33,23 +33,22 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		return (NULL);
 	}
-	else if (idx == 1)
-	{
-		new_node->next = *head;
-		*head = new_node;
-		i++;
-	}
-	else
-	{
+
 	/*uses loop to decrement by 1 in each iteration until idx becomes 0*/
-	while (--idx > 0)
+	while (position != NULL && i < idx - 1)
 	{
 		position = position->next;
+		i++;
 	}
+
+	if (position == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
 	new_node->next = position->next;
 	position->next = new_node;
-	i++;
-	}
 
 	return (new_node);
 }
