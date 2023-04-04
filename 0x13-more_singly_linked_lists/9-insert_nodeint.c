75c80f95530b6ee9b2fa027210ebe3945f5d5996
EucTech <euccreative@gmail.com>
2023-04-04 21:36:17 +0000

    inserted node at a given position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -33,7 +33,14 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		return (NULL);
 	}
-
+	else if (idx == 1)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		i++;
+	}
+	else
+	{
 	/*uses loop to decrement by 1 in each iteration until idx becomes 0*/
 	while (--idx > 0)
 	{
@@ -42,6 +49,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new_node->next = position->next;
 	position->next = new_node;
 	i++;
+	}
 
 	return (new_node);
 }
