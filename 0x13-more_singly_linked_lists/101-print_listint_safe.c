a1f3852fa575050b9ab7b09a5844c15b13a1a007
EucTech <euccreative@gmail.com>
2023-04-05 13:58:13 +0000

    printed a list

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * print_listint_safe - A functon that prints a list
+ * @head: The head
+ * Return: The number of nodes
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	const listint_t *node;
+	size_t length = 0;
+
+	while (head != NULL)
+	{
+		printf("[%p] %d\n", (void *)head, head->n);
+		length++;
+
+		node = head;
+		head = head->next;
+
+		if (node <= head)
+		{
+			printf("-> [%p] %d\n", (void *)head, head->n);
+			exit(98);
+		}
+	}
+	return (length);
+}
