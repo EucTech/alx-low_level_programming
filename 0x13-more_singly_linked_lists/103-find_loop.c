35f264fad5336c43bbe7be268ec7344f08c887e1
EucTech <euccreative@gmail.com>
2023-04-06 02:20:24 +0000

    fixed an error

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -20,18 +20,15 @@ listint_t *find_listint_loop(listint_t *head)
 	while (node1 && node2 && node2->next)
 	{
 		node1 = node1->next;
-		node2 = node2->next->next;
+		node2 = node2->next->next; /*move pointer to two node*/
 
+		/*if node1 and node2 is equal return the address of*/
+		/* the node the loop starts*/
 		if (node1 == node2)
 		{
-
-			while (node1 != node2)
-			{
-				node1 = node1->next;
-				node2 = node2->next;
-			}
 			return (node1);
 		}
+		head = node1;
 	}
 	return (NULL);
 }
