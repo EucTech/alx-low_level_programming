27d842083dc8eb36778a5528e2d2c657acfe8f0f
EucTech <euccreative@gmail.com>
2023-04-07 23:04:00 +0000

    function that read a text file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,31 +14,29 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t read_t;
-	char text[1000];
-
-	if (filename == NULL)
-		return (0);
+	ssize_t fd;
+	ssize_t read_t; 
+	char *buff_text;
 
 	fd = open(filename, O_RDONLY);
+	
 	if (fd == -1)
 		return (0);
 
-	read_t = read(fd, text, letters);
+	buff_text = malloc(sizeof(char) *letters + 1);
+	if (buff_text == NULL)
+		return (0);
+
+	read_t = read(fd, buff_text, letters);
 	if (read_t == -1)
-	{
-		close(fd);
+		return (0);
 
+	if (write(STDOUT_FILENO, buff_text, read_t) != read_t)
+	{
 		return (0);
 	}
 
-	if (write(STDOUT_FILENO, text, read_t) != read_t)
-	{
-		close(fd);
-
-		return (0);
-	}
+	free(buff_text);
 
 	close(fd);
 
