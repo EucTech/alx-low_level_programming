2c3fd7ad0fbcbfc7a0b5971d1da9548552ae63b9
EucTech <euccreative@gmail.com>
2023-04-10 10:35:00 +0000

    fixed error

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,19 +11,20 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int b_num = 0;
-	int i;
+	int i = 0;
 
 	if (b == NULL)
 		return (0);
 
-	for (i = 0; b[i] != '\0'; i++)
+	while (b[i] != '\0')
 	{
 		if (b[i] != '1' && b[i] != '0')
 			return (0);
-		
+	
 		b_num = b_num << 1;
 		if (b[i] == '1')
 			b_num |= 1;
+		i++;
 	}
 
 	return (b_num);
