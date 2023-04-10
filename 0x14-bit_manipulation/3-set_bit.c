62f31148c6e7e1da1867b197abfb931be58bb608
EucTech <euccreative@gmail.com>
2023-04-10 17:33:34 +0000

    betty style

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -2,16 +2,16 @@
 #include "main.h"
 
 /**
- * set_bet - a function that set valule of a bit to 1 at a given index
+ * set_bit - a function that set valule of a bit to 1 at a given index
  * @n: is a pointer to integer
  * @index: the index of the bit
  * Return: 1 if works or -1 if fails
  */
 
 int set_bit(unsigned long int *n, unsigned int index)
-{	
+{
 	unsigned long int num;
-	
+
 	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
 
