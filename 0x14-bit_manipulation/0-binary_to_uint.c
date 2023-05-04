52bd5e42a9e74433ddd030a1e0990f76abbb1df7
EucTech <euccreative@gmail.com>
2023-05-04 14:02:09 +0000

    a function that converts to binary

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * binary_to_uint - This is a function that converts a number to binary
+ * @b: This is a pointer to const char
+ * Return: The converted number
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int len;
+	unsigned int binary_num = 0;
+
+	if (b == NULL)
+		return (0);
+
+	len = 0;
+
+	while (b[len] != '\0')
+	{
+		if (b[len] != '1' && b[len] != '0')
+			return (0);
+
+		binary_num *= 2;
+
+		if (b[len] == '1')
+			binary_num |= 1;
+
+		len++;
+	}
+	
+	return (binary_num);
+}
