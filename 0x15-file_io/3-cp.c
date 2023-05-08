d4c4eaea33fb3dba0626c16aff87e90199c67d29
EucTech <euccreative@gmail.com>
2023-05-08 19:07:08 +0000

    a function to copy file from one to another

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,86 @@
+#include "main.h"
+
+/**
+ * to_copy_file_from_to - This is a function that copies content of a file
+ * @file_from: This is a char pointer to the source file
+ * @file_to: This is a char pointer to the destinatin of the file
+ * Return: The file copied
+ */
+void to_copy_file_from_to(char *file_from, char *file_to)
+{
+	int cf, ct;
+
+	cf = open(file_from, O_RDONLY);
+	if (cf == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
+	ct = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (ct == -1)
+	{
+		dprintf(2, "Error: Can't write to %s\n", file_to);
+		exit(99);
+	}
+
+	read_write(cf, ct);
+
+	if (close(cf) == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", cf);
+		exit(98);
+	}
+
+	if (close(ct) == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", ct);
+		exit(100);
+	}
+}
+
+/**
+ * read_write - This is a function that will write and read the file content
+ * @cf: The source
+ * @ct: The destination
+ * Return: void
+ */
+
+void read_write(int cf, int ct)
+{
+	char buffer[1024];
+	ssize_t to_write, to_read;
+
+	while ((to_read = read(cf, buffer, 1024)) > 0)
+	{
+		to_write = write(ct, buffer, to_read);
+		if (to_write == -1)
+		{
+			dprintf(2, "Error: Can't write to %d\n", ct);
+			exit(99);
+		}
+	}
+	if (to_read == -1)
+	{
+		dprintf(2, "Error: Can't read from file %d\n", cf);
+		exit(98);
+	}
+}
+
+/**
+ * main - This main checks arguments
+ * @ac: This is the number of argument counts
+ * @av: This is the argument vector
+ * Return: 0
+ */
+
+int main(int ac, char **av)
+{
+	if (ac != 3)
+	{
+		dprintf(2, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	to_copy_file_from_to(av[1], av[2]);
+
+	return (0);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,5 +10,8 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int main(int ac, char **av);
+void to_copy_file_from_to(char *file_from, char *file_to);
+void read_write(int cf, int ct);
 
 #endif
