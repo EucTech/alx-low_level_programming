cc2407a803cb0c7eeff866ea04e8f075e26dba6a
EucTech <euccreative@gmail.com>
2023-05-09 01:03:28 +0000

    main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,6 +6,9 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <elf.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
@@ -13,5 +16,17 @@ int append_text_to_file(const char *filename, char *text_content);
 int main(int ac, char **av);
 void to_copy_file_from_to(char *file_from, char *file_to);
 void read_write(int cf, int ct);
+
+/**** elf_header ****/
+
+void system_search(char *buff);
+int main(int ac, char **av);
+int elf_search(char *buff);
+void display_info(char *buff);
+void display_data( char *buff);
+void display_version(char *buff);
+void display_osabi(char *buff);
+void display_type(char *buff);
+void display_address(char *buff);
 
 #endif
