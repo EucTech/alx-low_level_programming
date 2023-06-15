71a05c8567286f84962392916b1a5aa7d10f0980
EucTech <euccreative@gmail.com>
2023-06-15 12:58:23 -0700

    function that returns an element

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count  = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
+
