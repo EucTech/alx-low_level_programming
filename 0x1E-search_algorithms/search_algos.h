ed01f64b15d510201ce1bea325ad7034ff37f5b3
EucTech <euccreative@gmail.com>
2023-10-10 23:13:40 +0100

    header file

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,11 @@
+#ifndef __SEARCH_ALGOS_H__
+#define __SEARCH_ALGOS_H__
+
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int linear_search(int *array, size_t size, int value);
+
+
+#endif
